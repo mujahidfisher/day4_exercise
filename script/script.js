c71ae4af42f20d7963e0df7095dfd64b7a3b7153
mujahidfisher <fishermujahid@gmail.com>
2023-05-26 10:01:18 +0200

    complete

---
+++ script/script.js
@@ -0,0 +1,15 @@
+// let person1 = {
+//     firstName: "Mujahid",
+//     lastName: "Fisher"
+// }
+
+// let person2 = {
+//     firstName: "Jake",
+//     lastName: "Kahn"
+// }
+
+// console.log(person1);
+// console.log(person2);
+
+let people = [person1, person2]
+console.log(people);
